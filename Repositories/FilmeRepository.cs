8d57021af04a80da2e35d7b119e3f6320a6c0253
Higor Silva <higor.silva14@aluno.senai.br>
2025-02-27 08:40:29 -0300

    Subindo Projeto API

---
+++ Repositories/FilmeRepository.cs
@@ -0,0 +1,102 @@
+﻿using api_filmes_senai.Context;
+using api_filmes_senai.Domains;
+using api_filmes_senai.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_filmes_senai.Repositories
+{
+    public class FilmeRepository : IFilmeRepository
+    {
+        private readonly Filmes_Context _context;
+
+        public FilmeRepository(Filmes_Context context)
+        {
+                _context = context;
+        }
+
+        public void Atualizar(Guid id, Filme filme)
+        {
+            Filme filmeBuscado = _context.Filmes.Find(id)!;
+
+            if (filmeBuscado != null)
+            {
+                filmeBuscado.Titulo = filme.Titulo;
+                filmeBuscado.IdGenero = filme.IdGenero;
+            }
+            _context.SaveChanges();
+        }
+
+        public Filme BuscarPorId(Guid id)
+        {
+            try
+            {
+                Filme filmeBuscado = _context.Filmes.Find(id)!;
+
+                return filmeBuscado;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void Cadastrar(Filme novoFilme)
+        {
+            try
+            {
+                _context.Filmes.Add(novoFilme);
+
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        public void Deletar(Guid id)
+        {
+            try
+            {
+                Filme filmeBuscado = _context.Filmes.Find(id)!;
+
+                if (filmeBuscado != null)
+                {
+                    _context.Filmes.Remove(filmeBuscado);
+                }
+
+                _context.SaveChanges();
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public List<Filme> Listar()
+        {
+            try
+            {
+                List<Filme> listaDeFilmes = _context.Filmes.ToList();
+                return listaDeFilmes;
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public static implicit operator FilmeRepository(Filmes_Context v)
+        {
+            throw new NotImplementedException();
+        }
+        public List<Filme> ListarPorGenero(Guid idGenero)
+        {
+            return _context.Filmes.Where(f => f.IdGenero == idGenero).ToList();
+        }
+
+    }
+}
